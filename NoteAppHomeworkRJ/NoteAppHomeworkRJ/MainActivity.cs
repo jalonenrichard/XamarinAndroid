4e5230bc302274a46a3134daf4f36340d52f6529
unknown <richard.jalonen@tptlive.ee>
2018-11-24 19:56:11 +0200

    initial commit, very basic functionality

---
+++ NoteAppHomeworkRJ/NoteAppHomeworkRJ/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Environment = Android.OS.Environment;
+
+namespace NoteAppHomeworkRJ
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        List<Note> noteList;
+
+        private static readonly log4net.ILog log
+            = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            NoteDao noteDao = new NoteDao(Path.Combine(
+                System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal),
+                "noteDatabase.db3"));
+            noteDao.CreateNewNoteTable();
+            var allNotes = noteDao.GetAllNotesFromDatabase();
+            try
+            {
+                noteList = allNotes.ToList();
+            }
+            catch (NullReferenceException e)
+            {
+                log.Info(e.Message);
+            }
+
+            var listView = FindViewById<ListView>(Resource.Id.listView1);
+            var editNotesButton = FindViewById<Button>(Resource.Id.buttonEditNote);
+
+            listView.Adapter = new CustomAdapter(this, noteList);
+            listView.ItemClick += (sender, e) => { Toast.MakeText(this, $"Vajutasid", ToastLength.Short).Show(); };
+
+            editNotesButton.Click += delegate
+            {
+                var editNoteActivity = new Intent(this, typeof(NewNotesActivity));
+                StartActivity(editNoteActivity);
+            };
+        }
+    }
+}
---
+++ NoteAppHomeworkRJ/NoteAppHomeworkRJ/obj/Debug/81/android/src/md550104a0db939c201e2bdd47d992b25d5/MainActivity.java
@@ -0,0 +1,47 @@
+package md550104a0db939c201e2bdd47d992b25d5;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("NoteAppHomeworkRJ.MainActivity, NoteAppHomeworkRJ", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("NoteAppHomeworkRJ.MainActivity, NoteAppHomeworkRJ", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
